bfc2e21dc1b74767ef768cb73b051468583509cd
b03201003 <b03201003@ntu.edu.tw>
2019-08-12 16:08:52 +0800

    .keep

--- src/game_manager.py
+++ src/game_manager.py
@@ -361,3 +361,14 @@ def puzzle_jugde(screen,GM,puzzle_object_id):
 #GM = GameManagerScreen()
 #pygame.init()
 
+
+# >>> def keep(path):
+# ...     dir = os.listdir(path)
+# ...     if len(dir) == 0:
+# ...             f = open(os.path.join(path,'.keep'),'w')
+# ...             f.close()
+# ...     for d in dir:
+# ...             if os.path.isdir(os.path.join(path,d)):
+# ...                     keep(os.path.join(path,d))
+# ...
+# >>> keep('./')
