15ddd3cffe506ef1e56bd73e31f65c7d2114327c
b03201003 <b03201003@ntu.edu.tw>
2019-08-13 18:11:03 +0800

    tables

--- src/object_table_parser.py
+++ src/object_table_parser.py
@@ -21,9 +21,8 @@ if os.path.isfile('res/allocate_all_objects_table.json'):
 object_count = 0
 function_names = {'道具':'item','解碼':'puzzle','開鎖':'lock','場景切換':'switching','觸發':'trigger','線索':'clue'}
 chapter_code = {'一':0,'二':1,'三':2,'四':3}
+player = {'A':0,'B':1,'C':2,'D':3}
 
-#for testing
-end_index = {0:24,1:35,2:35,3:19}
 for f in os.listdir(path):#0.csv,1.csv,2.csv,3.csv
 	if '.csv' in f:
 
@@ -32,8 +31,7 @@ for f in os.listdir(path):#0.csv,1.csv,2.csv,3.csv
 		print(df['物件一覽表'])
 
 		for i,object_name in enumerate(df['物件一覽表']):#TODO:把df['取得章節']之格式規範清楚
-			if i >= end_index[int(f[0])]:
-				break
+
 			if isinstance(object_name,float):
 				continue
 
@@ -65,7 +63,7 @@ for f in os.listdir(path):#0.csv,1.csv,2.csv,3.csv
 				else:
 					content['map_name'] = loc[1]  
 
-				content['player'] = int(f[0])
+				content['player'] = player[f[11]]
 
 				content['chapter'] = [0,1,2,3]
 
