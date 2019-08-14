5a7fe268a508469d76cdcd7f69eb9f45a97538d0
James Lin <b03201003@ntu.edu.tw>
2019-08-14 11:35:25 +0800

    fix map distributor

--- src/map_images_distributor.py
+++ src/map_images_distributor.py
@@ -20,6 +20,16 @@ print(abs_path)
 print(df)
 print(df.keys()[1:])
 
+for p in range(4):
+	for c in range(4):
+		map_dir = 'res/chapters/'+str(p)+'_'+str(c)+'/maps/'
+		if not os.path.isdir(map_dir):
+			os.mkdir(map_dir)
+		else:
+			for im in os.listdir(map_dir):
+				os.remove(os.path.join(map_dir, im))		
+
+
 not_found = []
 for k in df.keys()[1:]:
 	for name in df[k]:
@@ -28,10 +38,6 @@ for k in df.keys()[1:]:
 			print("map_name:",map_name)
 			p,c = int(name[0])-1,int(name[2])-1
 			map_dir = 'res/chapters/'+str(p)+'_'+str(c)+'/maps/'
-			if not os.path.isdir(map_dir):
-				os.mkdir(map_dir)
-			for im in os.listdir(map_dir):
-				os.remove(os.path.join(map_dir, im))
 
 			if len(map_name)==0:
 				continue
@@ -40,14 +46,7 @@ for k in df.keys()[1:]:
 			for im in imgs:
 				if map_name == im.split('.')[0]:
 					print(f'player:{p},chapter:{c},im:{im},map_name:{map_name} found')
-
-					#not support in Windows?
-					#shutil.copy(os.path.join('res/images/handpainting/',im),os.path.join(map_dir,im))# os.path.join(map_dir,im))
-					src = os.path.join('res\images\handpainting', im)
-					dst = os.path.join(map_dir,im).replace('/','\\')
-					print('src:',src)
-					print('dst:',dst)
-					os.system(f'copy {src} {dst}')
+					shutil.copy2(os.path.join('res/images/handpainting/',im),map_dir)# os.path.join(map_dir,im))
 					found = 1
 					
 			if not found:
